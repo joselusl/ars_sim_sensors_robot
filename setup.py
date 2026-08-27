9961666d71670b070edbfbd35c82faad5d443851
Jose Luis SANCHEZ-LOPEZ <joselusl@hotmail.com>
2026-08-27 17:27:08 +0200

    minor updates for compatibility

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
  maintainer_email='joseluis.sanlop@gmail.com',
  description='TODO: Package description',
  license='BSD',
- tests_require=['pytest'],
  entry_points={'console_scripts': [
  		'ars_sim_sensor_atti_robot_ros_node = ars_sim_sensors_robot.ars_sim_sensor_atti_robot_ros_node:main',
  		'ars_sim_sensor_posi_robot_ros_node = ars_sim_sensors_robot.ars_sim_sensor_posi_robot_ros_node:main',
